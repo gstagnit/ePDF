e9ffa1fd977bb67e8212b84e5cea297b7604f9b8
Valerio Bertone <valerio.bertone@cern.ch>
2019-11-28 10:27:43 +0100

    Adjusting banner

--- src/banner.cc
+++ src/banner.cc
@@ -20,7 +20,7 @@ namespace ePDF
     std::cout << "_____v" << VERSION << ": A code for the computation and the evolution of electron PDFs in QED at NLL\n";
     std::cout << "     webpage: https://github.com/gstagnit/ePDF\n";
     std::cout << "     Authors: V. Bertone, M. Cacciari, S. Frixione, G. Stagnitto\n";
-    std::cout << "     If you used this code, please cite:\n";
+    std::cout << "     If you use this code for a scientific publication, please cite:\n";
     std::cout << "     - S. Frixione, [arXiv:1909.03886]\n";
     std::cout << "     - V. Bertone, M. Cacciari, S. Frixione, G. Stagnitto, [arXiv:1911.12040]\n";
     std::cout << "\033[39m\n";
